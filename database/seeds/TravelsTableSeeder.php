3bfc513e7f24e0dc5212c4544649c7e733774129
silvio usai <berzylius@hotmail.it>
2021-06-17 17:09:10 +0200

    first commit

---
+++ database/seeds/TravelsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Travel;
+use Faker\Generator as Faker;
+
+class TravelsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++){
+            $newTravel = new Travel();
+            $newTravel->name = $faker->name;
+        }
+    }
+}
